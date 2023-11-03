a6500e63c6f1dca79b08efe3de17cec851d91d18
IsacJmx <142840259+IsacJmx@users.noreply.github.com>
2023-11-03 14:45:27 -0500

    Update Number_game.py

--- Number_game.py
+++ Number_game.py
@@ -14,8 +14,13 @@ def players():
 
 
 def get_number():
-    pass
-
+    
+    # get min and max numbers
+    Min = input("What is the minimum number? ")
+    Max = input('What is the maximum number? ')
+                
+    return Min
+    return Max
 
 def winner():
     pass
@@ -24,3 +29,21 @@ def winner():
 def turns():
     pass
 
+
+
+# Da_Number = '100'
+    # get the players guess
+    #number = input('Enter your number')
+    
+    #while number != Da_Number:
+    # if the number is right 
+        #if number != Da_Number:
+    #        print('That was wrong')
+     #       number  = input('what is guess? ')
+        
+      #      if number != Da_Number:
+       #         number = input('What is p1 guess: ')
+
+    #if number == Da_Number:
+     #   print('That was right')
+      #  print('player won')
